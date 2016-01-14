e0ae57ef8d9493070719a7ccdfc00b893352c450
senlinwuran <609815107@qq.com>
2016-01-15 00:00:42 +0800

    Initial Commit

---
+++ TestFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFramework
+//
+//  Created by LiuBo on 16/1/15.
+//  Copyright © 2016年 com.xuerersi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
